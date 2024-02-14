c6f698ba1e9f496ad435250e4fb352118d105920
Jacob Schrott <0062@htl.rennweg.at>
2024-02-14 10:51:43 +0100

    Transfer content from prototype.html to index.html. + User Story 2 fertig

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
-$username = "Hans Meiser";
+// Open the HTML file
+$file = fopen('./templates/index.html', 'r');
+// Read the content of the file
+$html = fread($file, filesize('./templates/index.html'));
+// Close the file
+fclose($file);
 
-$template = file_get_contents('./templates/index.html');
-$template = str_replace('###username###', $username, $template);
+// Define the placeholders and their corresponding replacements
+$placeholders = ['###hotel1###', '###hotel2###', '###hotel3###'];
+$replacements = ['Hotel Bellagio', 'Hotel Venetian', 'Hotel Caesar Palace'];
 
-echo $template;
+// Replace the placeholders with their corresponding values
+$result = str_replace($placeholders, $replacements, $html);
+
+// Echo the final result
+echo $result;
