7b191c3acfc92c7179acb5b721b77849f2d62615
Jacob Schrott <0062@htl.rennweg.at>
2024-02-14 16:59:31 +0100

    verwirrter commit

--- index.php
+++ index.php
@@ -7,11 +7,16 @@ $html = fread($file, filesize('./templates/index.html'));
 fclose($file);
 
 // Define the placeholders and their corresponding replacements
-$placeholders = ['###hotel1###', '###hotel2###', '###hotel3###'];
-$replacements = ['Hotel Bellagio', 'Hotel Venetian', 'Hotel Caesar Palace'];
+/*$placeholders = ['###hotel1###', '###hotel2###', '###hotel3###'];
+$replacements = ['Hotel Bellagio', 'Hotel Venetian', 'Hotel Caesar Palace'];*/
+
+$hotels = ['Hotel Bellagio', 'Hotel Venetian', 'Hotel Caesar Palace', 'Hotel Wynn', 'Hotel Paris'];
 
 // Replace the placeholders with their corresponding values
-$result = str_replace($placeholders, $replacements, $html);
+//$result = str_replace($placeholders, $replacements, $html);
+foreach ($hotels as $hotel) {
+    $result = str_replace('###hotel###', $hotel, $html);
+}
 
 // Echo the final result
 echo $result;
