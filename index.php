d927a10f86695e52caf403843eae11cce7e57d14
Jacob Schrott <0062@htl.rennweg.at>
2024-02-15 22:19:39 +0100

    user story 3 fertig!

--- index.php
+++ index.php
@@ -1,22 +1,34 @@
 <?php
-// Open the HTML file
-$file = fopen('./templates/index.html', 'r');
-// Read the content of the file
-$html = fread($file, filesize('./templates/index.html'));
-// Close the file
-fclose($file);
-
-// Define the placeholders and their corresponding replacements
-/*$placeholders = ['###hotel1###', '###hotel2###', '###hotel3###'];
-$replacements = ['Hotel Bellagio', 'Hotel Venetian', 'Hotel Caesar Palace'];*/
+// Define the placeholder
+$placeholder = '###hotel_cards###';
 
 $hotels = ['Hotel Bellagio', 'Hotel Venetian', 'Hotel Caesar Palace', 'Hotel Wynn', 'Hotel Paris'];
 
-// Replace the placeholders with their corresponding values
-//$result = str_replace($placeholders, $replacements, $html);
+// Initialize the result variable
+$result = '';
+
+// Iterate through each hotel and generate HTML for each
 foreach ($hotels as $hotel) {
-    $result = str_replace('###hotel###', $hotel, $html);
+    // HTML code for a single hotel card
+    $hotel_card = "
+        <div class='col-md-4 mt-3'>
+            <div class='card w-3'>
+                <div class='card-body'>
+                    <h5 class='card-title'>$hotel</h5>
+                    <p class='card-text'>Some description about $hotel here...</p>
+                </div>
+            </div>
+        </div>
+    ";
+    // Append the hotel card HTML to the result
+    $result .= $hotel_card;
 }
 
+// Read the content of the HTML template
+$html_template = file_get_contents('./templates/index.html');
+
+// Replace the placeholder with the hotel cards HTML
+$html_result = str_replace($placeholder, $result, $html_template);
+
 // Echo the final result
-echo $result;
+echo $html_result;
