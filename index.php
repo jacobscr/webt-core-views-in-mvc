d512c108fe7c5b2b26f161defa01b3752b203c62
Jacob Schrott <0062@htl.rennweg.at>
2024-02-15 23:09:17 +0100

    hotels als template gemacht

--- index.php
+++ index.php
@@ -7,29 +7,19 @@ $hotels = ['Hotel Bellagio', 'Hotel Venetian', 'Hotel Caesar Palace', 'Hotel Wyn
 // Initialize the result variable
 $result = '';
 
-// Iterate through each hotel and generate HTML for each
+$hotelfile = fopen('./templates/hotel.html', 'r');
+$hotel_template = fread($hotelfile, filesize('./templates/index.html'));
+fclose($hotelfile);
 foreach ($hotels as $hotel) {
-    // HTML code for a single hotel card
-    $hotel_card = "
-        <div class='col-md-4 mt-3'>
-            <div class='card w-3'>
-                <div class='card-body'>
-                    <h5 class='card-title'>$hotel</h5>
-                    <p class='card-text'>Some description about $hotel here...</p>
-                </div>
-            </div>
-        </div>
-    ";
-    // Append the hotel card HTML to the result
-    $result .= $hotel_card;
+    $result .= str_replace('###hotel_name###', $hotel, $hotel_template);
 }
 
 // Open the HTML file
-$file = fopen('./templates/index.html', 'r');
+$indexfile = fopen('./templates/index.html', 'r');
 // Read the content of the file
-$html_template = fread($file, filesize('./templates/index.html'));
+$html_template = fread($indexfile, filesize('./templates/index.html'));
 // Close the file
-fclose($file);
+fclose($indexfile);
 
 // Replace the placeholder with the hotel cards HTML
 $html_result = str_replace($placeholder, $result, $html_template);
