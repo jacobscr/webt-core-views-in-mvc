c2e5abb9d00bca152f7c4248b5d501bd5d7db71b
Jacob Schrott <0062@htl.rennweg.at>
2024-02-16 10:52:28 +0100

    webt-adv-twig-templating-engine fertig

--- index.php
+++ index.php
@@ -1,28 +1,17 @@
 <?php
+require 'vendor/autoload.php';
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 // Define the placeholder
 $placeholder = '###hotel_cards###';
 
 $hotels = ['Hotel Bellagio', 'Hotel Venetian', 'Hotel Caesar Palace', 'Hotel Wynn', 'Hotel Paris'];
 
-// Initialize the result variable
-$result = '';
+// Twig setup
+$loader = new FilesystemLoader('./views');
+$twig = new Environment($loader);
 
-$hotelfile = fopen('./templates/hotel.html', 'r');
-$hotel_template = fread($hotelfile, filesize('./templates/index.html'));
-fclose($hotelfile);
-foreach ($hotels as $hotel) {
-    $result .= str_replace('###hotel_name###', $hotel, $hotel_template);
-}
-
-// Open the HTML file
-$indexfile = fopen('./templates/index.html', 'r');
-// Read the content of the file
-$html_template = fread($indexfile, filesize('./templates/index.html'));
-// Close the file
-fclose($indexfile);
-
-// Replace the placeholder with the hotel cards HTML
-$html_result = str_replace($placeholder, $result, $html_template);
-
-// Echo the final result
-echo $html_result;
+// Render the index template with the hotel cards
+echo $twig->render('index.html', ['hotels' => $hotels]);
